2111b0a5c06e3c9b055eb9b7316ea14874396958
umar-yq <YaqoobU@cardiff.ac.uk>
2026-05-02 23:28:31 +0100

    Update DHT and Light sensor port configurations

--- Code/config.py
+++ Code/config.py
@@ -3,14 +3,14 @@ ACCESS_TOKEN = 'cZ9i29BrJiAMTJv3d99w'
 
 # Hardware setup
 
-# DHT Sensor 4 = D4 Port
-DHT_PORT = 4
+# DHT Sensor 8 = D8 Port
+DHT_PORT = 8
 
 # Sound Sensor = A0 Port
 SOUND_PORT = 0
 
-# Light Sensor = 8 Port
-LIGHT_PORT = 8
+# Light Sensor = 4 Port
+LIGHT_PORT = 4
 
 # Light Sensor Type = 0 = PIR
 LIGHT_TYPE = 0
